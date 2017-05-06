349142b5e54a7cc493be67bd996a20f3b9f1a7f6
Christopher <22csnyder@gmail.com>
2017-05-06 12:06:31 -0500

    Chris updates Causal_model to encorporate arbitrary causal graphs

---
+++ causal_graph.py
@@ -0,0 +1,105 @@
+'''
+To use a particular causal graph, just specify it here
+
+
+Strings specified have to match *exactly* to keys in attribute text file
+
+
+A graph lists each node and it's parents in pairs
+
+A->B, C->D, D->B:
+    [['A',[]],
+     ['B',['A','D']],
+     ['C',[]],
+     ['D',[]]]
+
+'''
+
+all_nodes=[
+        ['5_o_Clock_Shadow',[]],
+        ['Arched_Eyebrows',[]],
+        ['Attractive',[]],
+        ['Bags_Under_Eyes',[]],
+        ['Bald',[]],
+        ['Bangs',[]],
+        ['Big_Lips',[]],
+        ['Big_Nose',[]],
+        ['Black_Hair',[]],
+        ['Blond_Hair',[]],
+        ['Blurry',[]],
+        ['Brown_Hair',[]],
+        ['Bushy_Eyebrows',[]],
+        ['Chubby',[]],
+        ['Double_Chin',[]],
+        ['Eyeglasses',[]],
+        ['Goatee',[]],
+        ['Gray_Hair',[]],
+        ['Heavy_Makeup',[]],
+        ['High_Cheekbones',[]],
+        ['Male',[]],
+        ['Mouth_Slightly_Open',[]],
+        ['Mustache',[]],
+        ['Narrow_Eyes',[]],
+        ['No_Beard',[]],
+        ['Oval_Face',[]],
+        ['Pale_Skin',[]],
+        ['Pointy_Nose',[]],
+        ['Receding_Hairline',[]],
+        ['Rosy_Cheeks',[]],
+        ['Sideburns',[]],
+        ['Smiling',[]],
+        ['Straight_Hair',[]],
+        ['Wavy_Hair',[]],
+        ['Wearing_Earrings',[]],
+        ['Wearing_Hat',[]],
+        ['Wearing_Lipstick',[]],
+        ['Wearing_Necklace',[]],
+        ['Wearing_Necktie',[]],
+        ['Young',[]]
+    ]
+
+subset1_nodes=[
+        ['Bald',[]],
+#        ['Blurry',[]],
+#        ['Brown_Hair',[]],
+#        ['Bushy_Eyebrows',[]],
+#        ['Chubby',[]],
+        ['Double_Chin',[]],
+#        ['Eyeglasses',[]],
+#        ['Goatee',[]],
+#        ['Gray_Hair',[]],
+        ['Male',[]],
+        ['Mustache',[]],
+        ['No_Beard',[]],
+        ['Smiling',[]],
+#        ['Straight_Hair',[]],
+#        ['Wavy_Hair',[]],
+        ['Wearing_Earrings',[]],
+#        ['Wearing_Hat',[]],
+        ['Wearing_Lipstick',[]],
+        ['Young',[]]
+    ]
+
+
+standard_graph=[
+       ['Male'   , []              ],
+       ['Young'  , []              ],
+       ['Smiling', ['Male','Young']]
+       ]
+
+def get_causal_graph(causal_model=None,*args,**kwargs):
+
+
+    if causal_model is 'male.young.smiling':
+        graph=standard_graph
+    elif causal_model == 'subset1':
+        graph=subset1_nodes
+
+    if causal_model is 'empty':
+        graph=[[],[]]
+
+
+
+
+
+    return graph
